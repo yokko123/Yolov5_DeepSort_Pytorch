2a96ba10199de02d72066207dc9ebdd45dd47a09
Md Sazidur Rahman <mdsazidur02@gmail.com>
2022-01-26 22:49:02 +0600

    try5

--- track.py
+++ track.py
@@ -13,12 +13,12 @@ sys.path.insert(0, './yolov5')
 
 
 # from yolov5.utils.downloads import attempt_download
-from yolov5.models.common import DetectMultiBackend
-from yolov5.utils.datasets import LoadImages, LoadStreams
-from yolov5.utils.general import LOGGER, check_img_size, non_max_suppression, scale_coords, check_imshow, xyxy2xywh, \
+from models.common import DetectMultiBackend
+from utils.datasets import LoadImages, LoadStreams
+from utils.general import LOGGER, check_img_size, non_max_suppression, scale_coords, check_imshow, xyxy2xywh, \
     increment_path
-from yolov5.utils.torch_utils import select_device, time_sync
-from yolov5.utils.plots import Annotator, colors
+from utils.torch_utils import select_device, time_sync
+from utils.plots import Annotator, colors
 from deep_sort_pytorch.utils.parser import get_config
 from deep_sort_pytorch.deep_sort import DeepSort
 
